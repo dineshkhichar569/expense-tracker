62cd793c91f2f5031042881609565a81735e0b3e
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-09-20 04:48:13 +0530

    feat(client): add ChartComponent for visualizing transaction categories in analytics

---
+++ client/src/components/ChartComponent.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
+import { EXPENSE_CATEGORIES, INCOME_CATEGORIES } from "../utils/constants";
+
+/**
+ * To display the transaction category in donut chart
+ *
+ * @param {Object} props
+ * @param {Array} props.categories  transactions categories
+ * @param {number} props.total Total transaction amount
+ * @param {string} props.direction the layout directtion
+ * @returns {JSX.Element}
+ */
+function ChartComponent({ categories, total, direction }) {
+  return (
+    <div className="bg-white border border-[#EAE8E4] rounded-2xl p-4 min-w-md">
+      <h2 className="text-lg font-semibold">Category Split</h2>
+
+      <div
+        className="flex items-center gap-6 mt-4"
+        style={{ flexDirection: direction }}
+      >
+        {/* for Dount Chart */}
+        <div className="relative w-70 h-52 shrink-0">
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+              <Pie
+                data={categories}
+                dataKey="price"
+                nameKey="name"
+                innerRadius={60}
+                outerRadius={90}
+                paddingAngle={2}
+                stroke="white"
+                strokeWidth={2}
+              >
+                {categories.map((item) => {
+                  let color;
+                  for (const category of [
+                    ...INCOME_CATEGORIES,
+                    ...EXPENSE_CATEGORIES,
+                  ]) {
+                    if (category.name === item.name) {
+                      color = category.chartColor;
+                    }
+                  }
+
+                  return <Cell key={item.name} fill={color || "#6B7280"} />;
+                })}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+
+          {/* for the total in the middle */}
+          <div className="absolute inset-0 flex flex-col items-center justify-center pointer-events-none">
+            <span className="text-lg text-gray-400 uppercase">Total</span>
+            <span className="text-xl font-semibold">
+              ₹{total.toLocaleString("en-IN")}
+            </span>
+          </div>
+        </div>
+
+        {/* for Category List */}
+        <div className="w-full space-y-1">
+          {categories.map((item) => {
+            const percentage = Math.round((item.price / total) * 100);
+            let color;
+            for (const category of [
+              ...INCOME_CATEGORIES,
+              ...EXPENSE_CATEGORIES,
+            ]) {
+              if (category.name === item.name) {
+                color = category.chartColor;
+              }
+            }
+
+            return (
+              <div
+                key={item.name}
+                className="flex items-center justify-between gap-3 pb-2 border-b border-[#EAE8E4] last:border-b-0"
+              >
+                <div className="flex items-center gap-4">
+                  <span
+                    className={`w-2 h-2 rounded-full shrink-0`}
+                    style={{ backgroundColor: color }}
+                  />
+                  <span className="text-sm text-[#27272A] capitalize">
+                    {item.name}
+                  </span>
+                </div>
+                <div className="flex gap-4 items-center">
+                  <span className="text-sm text-[#27272A]">
+                    ₹{item.price.toLocaleString("en-IN")}
+                  </span>
+                  <span className="text-sm text-right w-10 text-[#27272A]">
+                    {percentage}%
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChartComponent;
--- client/src/pages/AnalyticsPage.jsx
+++ client/src/pages/AnalyticsPage.jsx
@@ -1,121 +1,40 @@
 import { TrendingDown, TrendingUp, Wallet2 } from "lucide-react";
-import React, { useEffect, useState } from "react";
-import { getExpense } from "../services/ExpenseService";
+import React, { useState } from "react";
 import { EXPENSE_CATEGORIES, INCOME_CATEGORIES } from "../utils/constants";
+import EmptyState from "../components/EmptyState";
+import ChartComponent from "../components/ChartComponent";
+import { getTransactionSummary } from "../utils/transactionUtils.js";
+import { useOutletContext } from "react-router-dom";
 
+/**
+ * it dispaly the analaytics page with income and expense
+ * 
+ * @returns {JSX.Element} Analytic Page
+ */
 function AnalyticsPage() {
-  const [transactions, setTransactions] = useState([]);
   const [type, setType] = useState("expense");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await getExpense();
-      setTransactions(res.data);
-    };
+  // it gets the transaction data and form states from MainLayout
+  const {transactions} = useOutletContext();
 
-    fetchData();
-  }, []);
-
-  let totalIncome = 0;
-  let totalExpense = 0;
-  let expenseCategories = [];
-  let incomeCategories = [];
-
-  for (const item of transactions) {
-    if (item.type === "income") {
-      totalIncome += item.amount;
-
-      const currCategory = incomeCategories.find(
-        (cat) => cat.name === item.category,
-      );
-      if (currCategory) {
-        currCategory.price += item.amount;
-      } else {
-        incomeCategories.push({
-          name: item.category,
-          price: item.amount,
-        });
-      }
-    }
-    if (item.type === "expense") {
-      totalExpense += item.amount;
-
-      const currCategory = expenseCategories.find(
-        (cat) => cat.name === item.category,
-      );
-      if (currCategory) {
-        currCategory.price += item.amount;
-      } else {
-        expenseCategories.push({
-          name: item.category,
-          price: item.amount,
-        });
-      }
-    }
-  }
-
-  let netSaving = totalIncome - totalExpense;
+  const {
+    incomeCategories,
+    expenseCategories,
+    totalExpense,
+    totalIncome,
+    netSaving,
+  } = getTransactionSummary({ transactions });
 
   const topCategories =
-    type === "income"
-      ? incomeCategories.sort((a, b) => b.price - a.price)
-      : expenseCategories.sort((a, b) => b.price - a.price);
+    type === "income" ? incomeCategories : expenseCategories;
 
   return (
-    <div>
-      <div className="flex items-center justify-between gap-5">
-        <div className="h-24 w-full bg-white px-5 rounded-[20px] flex items-center gap-3.5">
-          <div className="w-12 h-12 rounded-xl bg-[#ccfbdc] text-[#16A34A] flex items-center justify-center shrink-0">
-            <TrendingUp />
-          </div>
-          <div>
-            <p className="text-[12px] text-gray-400 tracking-[0.06] uppercase">
-              Total Income
-            </p>
-            <p className="text-[28px] font-semibold text-[#16A34A] leading-none mt-1 tracking-[0.06] uppercase">
-              ₹{totalIncome.toLocaleString("en-In")}
-            </p>
-          </div>
-          <p className="ml-auto text-sm text-gray-500">+45%</p>
-        </div>
-        <div className="h-24 w-full bg-white px-5 rounded-[20px] flex items-center gap-3.5">
-          <div className="w-12 h-12 rounded-xl bg-[#FFE4E6] text-[#E11D48] flex items-center justify-center shrink-0">
-            <TrendingDown />
-          </div>
-          <div>
-            <p className="text-[12px] text-gray-400 tracking-[0.06em] uppercase">
-              Total Expense
-            </p>
-            <p className="text-[28px] font-semibold text-[#E11D48] leading-none mt-1 tracking-[0.06em] uppercase">
-              ₹{totalExpense.toLocaleString("en-In")}
-            </p>
-          </div>
-          <p className="ml-auto text-sm text-gray-500">-3.1%</p>
-        </div>
-        <div className="h-24 w-full bg-black px-5 rounded-[20px] flex items-center gap-3.5">
-          <div className="w-12 h-12 rounded-xl bg-[#c6c7c7] text-black flex items-center justify-center shrink-0">
-            <Wallet2 />
-          </div>
-          <div>
-            <p className="text-[12px] text-gray-400 tracking-[0.06] uppercase">
-              Net Saving
-            </p>
-            <p className="text-[28px] font-semibold text-white leading-none mt-1 tracking-[0.06] uppercase">
-              ₹{netSaving.toLocaleString("en-In")}
-            </p>
-          </div>
-          <p className="ml-auto text-sm text-gray-500">+45%</p>
-        </div>
-      </div>
-
-      {/* for charts and graphs */}
-      <div></div>
-
-      <div className="bg-white border border-[#EAE8E4] rounded-3xl px-8 py-4 flex flex-col gap-3">
-        <h2 className="font-semibold text-xl">Top Categories</h2>
-
+    <div className="flex flex-col gap-6">
+      {/* top header */}
+      <div className="flex justify-between items-center">
+        <h1 className="font-semibold text-2xl">Analytics</h1>
         {/* expense and income switch buttons */}
-        <div className="flex rounded-xl bg-[#F1F0ED] p-1 w-52">
+        <div className="flex rounded-xl bg-[#cacaca] p-1 w-52">
           <button
             type="button"
             onClick={() => setType("expense")}
@@ -131,57 +50,118 @@ function AnalyticsPage() {
             Income
           </button>
         </div>
+      </div>
+
+      {/* for total income and expense ans net saving */}
+      <div className="flex items-center justify-between gap-5">
+        <div className="h-24 w-full bg-white px-5 rounded-[20px] flex items-center gap-3.5">
+          <div className="w-12 h-12 rounded-xl bg-[#ccfbdc] text-[#16A34A] flex items-center justify-center shrink-0">
+            <TrendingUp />
+          </div>
+          <div>
+            <p className="text-[12px] text-gray-400 tracking-[0.06] uppercase">
+              Total Income
+            </p>
+            <p className="text-[28px] font-semibold text-[#16A34A] leading-none mt-1 tracking-[0.06] uppercase">
+              ₹{totalIncome.toLocaleString("en-In")}
+            </p>
+          </div>
+        </div>
+        <div className="h-24 w-full bg-white px-5 rounded-[20px] flex items-center gap-3.5">
+          <div className="w-12 h-12 rounded-xl bg-[#FFE4E6] text-[#E11D48] flex items-center justify-center shrink-0">
+            <TrendingDown />
+          </div>
+          <div>
+            <p className="text-[12px] text-gray-400 tracking-[0.06em] uppercase">
+              Total Expense
+            </p>
+            <p className="text-[28px] font-semibold text-[#E11D48] leading-none mt-1 tracking-[0.06em] uppercase">
+              ₹{totalExpense.toLocaleString("en-In")}
+            </p>
+          </div>
+        </div>
+        <div className="h-24 w-full bg-black px-5 rounded-[20px] flex items-center gap-3.5">
+          <div className="w-12 h-12 rounded-xl bg-[#c6c7c7] text-black flex items-center justify-center shrink-0">
+            <Wallet2 />
+          </div>
+          <div>
+            <p className="text-[12px] text-gray-400 tracking-[0.06] uppercase">
+              Net Saving
+            </p>
+            <p className="text-[28px] font-semibold text-white leading-none mt-1 tracking-[0.06] uppercase">
+              ₹{netSaving.toLocaleString("en-In")}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* for charts and graphs */}
+      <div className="">
+        <ChartComponent
+          categories={topCategories}
+          total={type === "income" ? totalIncome : totalExpense}
+          direction="row"
+        />
+      </div>
+
+      {/* for the categories in percentage wise */}
+      <div className="bg-white border border-[#EAE8E4] rounded-3xl px-8 py-4 flex flex-col gap-4">
+        <h2 className="font-semibold text-xl">Top Categories</h2>
 
         <div className="flex flex-col gap-2">
-          {topCategories.map((item) => {
-            const categories =
-              type === "income" ? INCOME_CATEGORIES : EXPENSE_CATEGORIES;
-            const category = categories.find((cat) => cat.name === item.name);
-            const Icon = category?.icon;
+          {topCategories.length === 0 ? (
+            <EmptyState />
+          ) : (
+            topCategories.map((item) => {
+              const categories =
+                type === "income" ? INCOME_CATEGORIES : EXPENSE_CATEGORIES;
+              const category = categories.find((cat) => cat.name === item.name);
+              const Icon = category?.icon;
+              const percentage = Math.round(
+                (item.price /
+                  (type === "income" ? totalIncome : totalExpense)) *
+                  100,
+              );
 
-            return (
-              <div
-                key={item.name}
-                className="flex items-center justify-between gap-5 border-b pb-2 border-[#EAE8E4] last:border-b-0"
-              >
-                <div className="flex items-center gap-5 w-1/6">
-                  <div
-                    className={`w-8 h-8 rounded-lg flex items-center justify-center ${category.bgColor}`}
-                  >
-                    <Icon size={16} className={category.color} />
+              return (
+                <div
+                  key={item.name}
+                  className="flex items-center justify-between gap-5 border-b pb-2 border-[#EAE8E4] last:border-b-0"
+                >
+                  <div className="flex items-center gap-5 w-1/6">
+                    <div
+                      className={`w-8 h-8 rounded-lg flex items-center justify-center ${category.bgColor}`}
+                    >
+                      <Icon size={16} className={category.color} />
+                    </div>
+                    <span className="capitalize text-sm font-medium">
+                      {item.name}
+                    </span>
                   </div>
-                  <span className="capitalize text-sm font-medium">
-                    {item.name}
-                  </span>
-                </div>
 
-                {/* for the progess bar */}
-                <div className="w-full flex-1 h-2.5 bg-[#F0EFED] rounded-full overflow-hidden">
-                  <div
-                    className={`h-full rounded-full ${category.barColor}`}
-                    style={{
-                      width: `${Math.round((item.price / (type === "income" ? totalIncome : totalExpense)) * 100)}%`,
-                    }}
-                  />
-                </div>
+                  {/* for the progess bar */}
+                  <div className="w-full flex-1 h-2.5 bg-[#F0EFED] rounded-full overflow-hidden">
+                    <div
+                      className={`h-full rounded-full ${category.barColor}`}
+                      style={{
+                        width: `${percentage}%`,
+                      }}
+                    />
+                  </div>
 
-                {/* for amount and percentage */}
-                <div className="w-1/10 flex items-center gap-5 text-sm font-medium">
-                  <span className="text-right">
-                    {item.price.toLocaleString("en-In")}
-                  </span>
-                  <span className="text-right text-[#6B6F76]">
-                    {Math.round(
-                      (item.price /
-                        (type === "income" ? totalIncome : totalExpense)) *
-                        100,
-                    )}{" "}
-                    %
-                  </span>
+                  {/* for amount and percentage */}
+                  <div className="w-1/9 flex items-center gap-5 text-sm font-medium">
+                    <span className="text-right">
+                      {item.price.toLocaleString("en-In")}
+                    </span>
+                    <span className="text-right text-[#6B6F76]">
+                      {percentage}%
+                    </span>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          )}
         </div>
       </div>
     </div>
