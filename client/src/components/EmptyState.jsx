4cff79af66385d6ff1ee6e0cfad251257a0e7a95
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-09-19 13:08:33 +0530

    feat(client): add EmptyState component to display message when no transactions exist

---
+++ client/src/components/EmptyState.jsx
@@ -0,0 +1,21 @@
+import { ReceiptText } from "lucide-react";
+import React from "react";
+
+/**
+ * It shows Message when there is no transaction
+ */
+function EmptyState() {
+  return (
+    <div className="flex flex-col items-center justify-centertext-center py-16">
+      <ReceiptText size={110} className="text-gray-400 mb-8" />
+      <h2 className="text-4xl font-semibold text text-gray-700">
+        No transactions yet
+      </h2>
+      <p className="text-2xl text-gray-500 mt-5">
+        Add your first expense to see your spending breakdown
+      </p>
+    </div>
+  );
+}
+
+export default EmptyState;
